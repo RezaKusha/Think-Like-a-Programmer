1309aacb5565c1f3959487e04911832a2b0da793
RezaKusha <reza.kusha1@gmail.com>
2023-11-18 18:32:46 +0330

    Finish exersice 5-2

---
+++ Chapter5/Exersice 5-2/Automobile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Automobile
+{
+    public class Automobile
+    {
+        private string _name;
+        private string _companyName;
+        private int _yearCreated;
+        private const int _presentYear = 2023;
+        
+        public int GetAge()
+        {
+            int age = _presentYear - _yearCreated;
+            return age;
+        }
+
+        public void CompleteInformation(string companyName, string name, int year)
+        {
+            _companyName = companyName;
+            _name = name;
+            _yearCreated = year;
+            Console.Write($"{_companyName} {_name} {_yearCreated} ");
+        }
+    }
+}
---
+++ Chapter5/Exersice 5-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Automobile
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Automobile car = new Automobile();
+            Automobile car2 = new Automobile();
+            
+            car.CompleteInformation("BMW","Z4",1999);
+            Console.Write(car.GetAge());
+            Console.WriteLine();
+            
+            car2.CompleteInformation("Chevrolet","Impala",1965);
+            Console.Write(car2.GetAge());
+            Console.WriteLine();
+        }
+    }
+}
