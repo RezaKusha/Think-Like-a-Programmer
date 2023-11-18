344328210a954720b04055b97c033179804cc953
RezaKusha <reza.kusha1@gmail.com>
2023-11-18 18:32:31 +0330

    Finish exercise 5-1

---
+++ Chapter5/Exersice 5-1/Automobile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Automobile
+{
+    public class Automobile
+    {
+        private string _name;
+        private string _companyName;
+        private int _yearCreated;
+
+        public string GetName()
+        {
+            return _name;
+        }
+        
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        
+        public string GetCompanyName()
+        {
+            return _companyName;
+        }
+        
+        public void SetCompanyName(string companyName)
+        {
+            _companyName = companyName;
+        }
+        
+        public int GetYear()
+        {
+            return _yearCreated;
+        }
+
+        public void SetYear(int year)
+        {
+            _yearCreated = year;
+        }
+    }
+}
---
+++ Chapter5/Exersice 5-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Automobile
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Automobile car = new Automobile();
+            Automobile car2 = new Automobile();
+            
+            car.SetName("peykan");
+            car.SetYear(1980);
+            car.SetCompanyName("IKCO");
+            
+            car2.SetName("S500");
+            car2.SetYear(2015);
+            car2.SetCompanyName("Benz");
+
+            Console.WriteLine($"{car.GetCompanyName()} {car.GetName()} {car.GetYear()}");
+            Console.WriteLine($"{car2.GetCompanyName()} {car2.GetName()} {car2.GetYear()}");
+        }
+    }
+}
